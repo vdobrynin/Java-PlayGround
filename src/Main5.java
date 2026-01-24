e47ff1c1a25891f6a7d3fd478c0217c0f031b569
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2026-01-23 19:42:51 -0800

    2nd commit

--- src/Main5.java
+++ src/Main5.java
@@ -1,14 +1,27 @@
 public class Main5 {
-    public static int addDigits(int num) {
+   /* public static int addDigits(int num) {
         if (num == 0) {
             return 0;
         } else {
             return (num - 1) % 9 + 1;
         }
-    }
-
+    }*/
+        public static int addDigits(int num) {
+            num = Math.abs(num); // Handle negative numbers
+            while (num >= 10) {
+                int sum = 0;
+                while (num > 0) {
+                    sum = sum + num % 10;
+//                    sum += num % 10;
+                    num = num / 10;
+//                    num /= 10;
+                }
+                num = sum;
+            }
+            return num;
+        }
     public static void main(String[] args) {
-        int n = 29;
+        int n = 15;
         System.out.println(addDigits(n));
     }
 }
